2f9352aca96172f24ba137951c4464154a7a9db8
88dldl <110217133+88dldl@users.noreply.github.com>
2022-08-02 14:22:49 +0900

    Update number3.java

--- number3.java
+++ number3.java
@@ -2,6 +2,6 @@ public class number3 {
     public static void main(String[] args){
         String hong="881120-1068234";
         System.out.print(hong.substring(0,6)+"\r\n");
-        System.out.println(hong.substring(7,14));
+        System.out.println(hong.substring(7));
     }
 }
