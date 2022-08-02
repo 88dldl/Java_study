ddd0db878db597440c2f44ff83ee5b38dd52c314
88dldl <110217133+88dldl@users.noreply.github.com>
2022-08-02 14:30:24 +0900

    2주차 과제

---
+++ number3.java
@@ -0,0 +1,7 @@
+public class number3 {
+    public static void main(String[] args){
+        String hong="881120-1068234";
+        System.out.print(hong.substring(0,6)+"\r\n");
+        System.out.println(hong.substring(7));
+    }
+}
