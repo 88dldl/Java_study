36778a7bab1642c87c340c3f2a2c6aba137800c4
88dldl <110217133+88dldl@users.noreply.github.com>
2022-08-08 04:52:33 +0900

    Delete Number3.java

--- Number3.java
+++
@@ -1,7 +0,0 @@
-public class Number3 {
-    public static void main(String[] args){
-        String hong="881120-1068234";
-        System.out.print(hong.substring(0,6)+"\r\n");
-        System.out.println(hong.substring(7));
-    }
-}
