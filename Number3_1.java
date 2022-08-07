61bec967221f8e9f5f1b8b2be38942ae01176019
88dldl <110217133+88dldl@users.noreply.github.com>
2022-08-08 05:01:23 +0900

    3장 문제풀이

---
+++ Number3_1.java
@@ -0,0 +1,10 @@
+public class Number3_1 {
+    public static void main(String[] args) {
+        int[] grade = {80, 75, 55};
+        double sum = 0;
+        for (int i = 0; i < 3; i++) {
+            sum += grade[i];
+        }
+        System.out.println(sum / 3);
+    }
+}
