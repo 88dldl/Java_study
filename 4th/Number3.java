1f3669797efed3bcc7d8666dba9bdf7837aa8dfe
88dldl <110217133+88dldl@users.noreply.github.com>
2022-08-18 18:57:10 +0900

    4주차 과제

---
+++ 4th/Number3.java
@@ -0,0 +1,24 @@
+class Song {
+    String title;
+    String singer;
+    int year;
+    String country;
+
+    Song(String title, String singer, int year, String country) {
+        this.title = title;
+        this.singer = singer;
+        this.year = year;
+        this.country = country;
+    }
+
+    void show() {
+        System.out.println(year + "년 " + country + "국적의 " + singer + "가 부른 " + title);
+    }
+}
+
+public class Number3 {
+    public static void main(String[] args) {
+        Song DancingQueen = new Song("Dancing Queen", "ABBA", 1978, "스웨덴");
+        DancingQueen.show();
+    }
+}
